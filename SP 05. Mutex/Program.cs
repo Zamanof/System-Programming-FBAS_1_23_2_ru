08aabc48cde34f6dd70a29eabfc084b0a2e4e02d
Nadir Zamanov <Zamanov@itstep.edu.az>
2024-08-27 17:58:01 +0400

    SP 05. Semaphore Mutex

---
+++ SP 05. Mutex/Program.cs
@@ -0,0 +1,45 @@
+﻿// lock/monitor - lock mechanism
+// Mutex, Semaphore, SemaphoreSlim - Signaling mechanism
+
+#region Mutex - Mutual exclusion (Internal thread)
+//Mutex mutex = new Mutex();
+
+//int numb = 1;
+//for (int i = 0; i < 5; i++)
+//{
+//    Thread thread = new(Count);
+//    thread.Name = $"Thread number {i}";
+//    thread.Start();
+//}
+//Console.ReadKey();
+
+//void Count()
+//{
+//    mutex.WaitOne();
+//    for (int i = 0; i< 10; i++)
+//    {
+//        Console.WriteLine($"{Thread.CurrentThread.Name}: {numb++}");
+//    }
+//    mutex.ReleaseMutex();
+//}
+#endregion
+
+#region Global Mutex - Mutual exclusion (External thread)
+
+string MutexName = "Rasilla";
+using var mutex = new Mutex(false, MutexName);
+
+if (!mutex.WaitOne(30000))
+{
+    Console.WriteLine("Other instance is running...");
+    Thread.Sleep(1000);
+    return;
+}
+else
+{
+    Console.WriteLine("My Code is running");
+    Console.ReadKey();
+    mutex.ReleaseMutex();
+}
+
+#endregion
---
+++ SP 05. Semaphore, SemaphoreSlim/Program.cs
@@ -0,0 +1,119 @@
+﻿#region Semaphore internal
+//Semaphore semaphore = new Semaphore(3, 3);
+
+//for (int i = 0; i < 15; i++)
+//{
+//    ThreadPool.QueueUserWorkItem(Some, semaphore);
+//}
+//Console.ReadKey();
+//void Some(object? state)
+//{
+//    var s = state as Semaphore;
+//    bool st = false;
+//    while (!st)
+//    {
+//        if (s.WaitOne(500))
+//        {
+//            try
+//            {
+//                Console.ForegroundColor = ConsoleColor.Red;
+//                Console.WriteLine($"Mr. {Thread.CurrentThread.ManagedThreadId} take key");
+//                Thread.Sleep(3000);
+
+//            }
+//            finally
+//            {
+//                Console.ForegroundColor = ConsoleColor.Green;
+//                st = true;
+//                Console.WriteLine($"Mr. {Thread.CurrentThread.ManagedThreadId} return key");
+//                s.Release();
+//            }
+//        }
+//        else
+//        {
+//            Console.ForegroundColor = ConsoleColor.White;
+//            Console.WriteLine($"Mr. {Thread.CurrentThread.ManagedThreadId}, sorry no keys yet");
+//        }
+//    }
+//}
+#endregion
+
+#region Semaphore external
+//Semaphore semaphore = new Semaphore(3, 3,"Jamalonguma");
+
+//for (int i = 0; i < 15; i++)
+//{
+//    ThreadPool.QueueUserWorkItem(Some, semaphore);
+//}
+//Console.ReadKey();
+//void Some(object? state)
+//{
+//    var s = state as Semaphore;
+//    bool st = false;
+//    while (!st)
+//    {
+//        if (s.WaitOne(500))
+//        {
+//            try
+//            {
+//                Console.ForegroundColor = ConsoleColor.Red;
+//                Console.WriteLine($"Mr. {Thread.CurrentThread.ManagedThreadId} take key");
+//                Thread.Sleep(10000);
+
+//            }
+//            finally
+//            {
+//                Console.ForegroundColor = ConsoleColor.Green;
+//                st = true;
+//                Console.WriteLine($"Mr. {Thread.CurrentThread.ManagedThreadId} return key");
+//                s.Release();
+//            }
+//        }
+//        else
+//        {
+//            Console.ForegroundColor = ConsoleColor.White;
+//            Console.WriteLine($"Mr. {Thread.CurrentThread.ManagedThreadId}, sorry no keys yet");
+//        }
+//    }
+//}
+#endregion
+
+#region SemaphoreSlim external
+SemaphoreSlim semaphore = new SemaphoreSlim(3, 3);
+
+for (int i = 0; i < 15; i++)
+{
+    ThreadPool.QueueUserWorkItem(Some, semaphore);
+}
+Console.ReadKey();
+void Some(object? state)
+{
+    var s = state as SemaphoreSlim;
+    bool st = false;
+    while (!st)
+    {
+        if (s.Wait(500))
+        {
+            try
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine($"Mr. {Thread.CurrentThread.ManagedThreadId} take key");
+                Thread.Sleep(10000);
+
+            }
+            finally
+            {
+                Console.ForegroundColor = ConsoleColor.Green;
+                st = true;
+                Console.WriteLine($"Mr. {Thread.CurrentThread.ManagedThreadId} return key");
+                s.Release();
+            }
+        }
+        else
+        {
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine($"Mr. {Thread.CurrentThread.ManagedThreadId}, sorry no keys yet");
+        }
+    }
+}
+#endregion
